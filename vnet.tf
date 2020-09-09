c4c1fff08c31f1030a2025d7fd1905d45176d74e
Espen Komissar Fossheim <digresiv@gmail.com>
2020-09-09 13:20:26 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,13 +1,23 @@
-module "network" {
-    source              = "Azure/network/azurerm"
-    resource_group_name = "myapp"
-    location            = "westus"
-    address_space       = "10.0.0.0/16"
-    subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    subnet_names        = ["subnet1", "subnet2", "subnet3"]
+provider "azurerm" {
+  features {}
+}
 
-    tags                = {
-                            environment = "dev"
-                            costcenter  = "it"
-                          }
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
 }
