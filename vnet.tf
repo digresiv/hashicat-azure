4e15d3e0d04fac95a4508d222db9413b2be5c69d
Espen Komissar Fossheim <digresiv@gmail.com>
2020-09-09 13:31:42 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -5,10 +5,11 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "network" {
+  version = "2.0.0"
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_space       = "10.0.0.0/16"
-  subnet_prefix     = "10.0.1.0/24"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   tags = {
